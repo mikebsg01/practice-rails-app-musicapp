71139e58dd15e0ebe9cb690bf74483efcf42904b
Michael Serrato <mikebsg01@gmail.com>
2018-07-16 00:28:26 -0500

    Added: [Scaffold] Track

---
+++ db/migrate/20180716051316_create_tracks.rb
@@ -0,0 +1,11 @@
+class CreateTracks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tracks do |t|
+      t.string :title
+      t.string :album
+      t.string :artist
+
+      t.timestamps
+    end
+  end
+end
